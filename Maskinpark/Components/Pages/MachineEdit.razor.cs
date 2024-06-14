734421c78224187ab24cb91acf2f9a8695fdadb2
fionaliu66 <fiona.liu0606@gmail.com>
2024-06-14 11:50:51 +0200

    Edit Machine

--- Maskinpark/Components/Pages/MachineEdit.razor.cs
+++ Maskinpark/Components/Pages/MachineEdit.razor.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Maskinpark.Models;
+using Maskinpark.Services;
+using Microsoft.AspNetCore.Components;
 
 namespace Maskinpark.Components.Pages
 {
@@ -6,5 +8,36 @@ namespace Maskinpark.Components.Pages
     {
         [Parameter]
         public int Id {  get; set; }
-    }
+        [SupplyParameterFromForm]
+        public Machine Machine { get; set; }
+
+		[Inject]
+        public MockDataService MockDataService {  get; set; }
+		[Inject]
+		public NavigationManager NavigationManager { get; set; }
+		protected bool IsSaved;
+		protected string Message = string.Empty;
+		protected string StatusClass = string.Empty;
+
+        protected override async Task OnInitializedAsync()
+        {
+			Machine = await MockDataService.GetMachineAsync(Id);
+        }
+
+        protected async Task HandleValidSubmit()
+		{
+		
+		}
+
+		protected void HandleInvalidSubmit()
+		{
+			StatusClass = "alert-danger";
+			Message = "There are some validation errors. Please try again.";
+
+		}
+		protected void NavigateToOverview()
+		{
+			NavigationManager.NavigateTo("/employeeoverview");
+		}
+	}
 }
