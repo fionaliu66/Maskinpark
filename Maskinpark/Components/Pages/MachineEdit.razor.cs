285a0594bbd183a2a6e58add4fab658859408a7c
fionaliu66 <fiona.liu0606@gmail.com>
2024-06-14 11:21:26 +0200

    No more detail

---
+++ Maskinpark/Components/Pages/MachineEdit.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Maskinpark.Components.Pages
+{
+    public partial class MachineEdit
+    {
+        [Parameter]
+        public int Id {  get; set; }
+    }
+}
